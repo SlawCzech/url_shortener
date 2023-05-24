3c735df1271c4877f62b5a476c7888e14dac3f54
SlawCzech <slaw.czech@gmail.com>
2023-05-24 21:43:36 +0200

    change url column type

--- api/shortener/models.py
+++ api/shortener/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class URL(models.Model):
-    original_url = models.CharField(max_length=2000)
+    original_url = models.URLField(max_length=2000)
     short_url = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
 
