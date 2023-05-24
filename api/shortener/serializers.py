88d7570d8b900649d898b2c73ac539b15eeeb18e
SlawCzech <slaw.czech@gmail.com>
2023-05-24 13:09:44 +0200

    docstrings revised

--- api/shortener/serializers.py
+++ api/shortener/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
 import shortuuid
+from rest_framework import serializers
 
 from .models import URL
 
--- api/shortener/views.py
+++ api/shortener/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from rest_framework.generics import get_object_or_404
 from django.views.generic import RedirectView
 from rest_framework import generics, status
@@ -14,6 +16,24 @@ class CreateShortURLApiView(generics.CreateAPIView):
     serializer_class = serializers.OriginalURLSerializer
 
     def create(self, request, *args, **kwargs):
+        """
+        create:
+        Creates a short URL from the provided original URL.
+
+        Expects JSON data in the format:
+        {
+            "url": "http://example.com"
+        }
+
+        Where "url" is the original URL that is to be shortened.
+
+        The response contains the original URL and the shortened URL:
+        {
+            "url": "http://example.com",
+            "short_url": "http://your-domain.com/abcd"
+        }
+        """
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         url = serializer.save()
@@ -24,7 +44,19 @@ class CreateShortURLApiView(generics.CreateAPIView):
 
 
 class ShortUrlRedirectView(RedirectView):
-    def get_redirect_url(self, *args, short_url, **kwargs):
+    """
+    get:
+    Redirects the user to the original URL from the provided short URL.
+
+    Expects the short URL to be part of the path, like:
+    http://your-domain.com/abcd
+
+    Where "abcd" is the short URL.
+
+    The response is a HTTP 302 redirect to the original URL.
+    """
+
+    def get_redirect_url(self, *args: Any, short_url: str, **kwargs: Any) -> str | None:
         url = get_object_or_404(URL, short_url=short_url)
 
         return url.original_url
